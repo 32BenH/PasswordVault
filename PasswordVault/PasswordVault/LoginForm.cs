ecf5ba3920e568a6f535040e2f3ddead2d951dd5
Ben <32benhayden@gmail.com>
2020-06-03 18:45:17 -0400

    Initial commit

---
+++ PasswordVault/PasswordVault/LoginForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PasswordVault {
+    public partial class LoginForm : Form {
+        public LoginForm() {
+            InitializeComponent();
+        }
+    }
+}
